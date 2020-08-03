6ca9c84d9093a98f73a4244d793d049e5eb27941
Yogesh <yvaliya4@gmail.com>
2020-08-03 12:33:01 +0530

    Implement basic EJS

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.get("/", (req, resp) => {
-    resp.end("Hello World...!")
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render("home", {layout: false})
 })
 
 app.listen(PORT, () => {
