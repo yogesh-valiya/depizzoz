26b27eaf7a2e26fa2b7bc7961572f6bd796cf98e
Yogesh <yvaliya4@gmail.com>
2020-08-03 18:17:04 +0530

    Added MongoDB connection.

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
+const mongo = require('mongoose')
+const mongoCred = require("./MongoDBCreds")
+
+const connectionString = mongoCred.MONGODB_CONNECTION_URL
+
+mongo.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+const connection = mongo.connection
+connection.once('open', () => {
+    console.log("Database connected...")
+}).catch(err => {
+    console.log("Connection failed...")
+    console.log(err)
+})
 
 const PORT = process.env.PORT || 3000
 
